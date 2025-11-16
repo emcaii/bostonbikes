03f48ee954e79f9178c6acaed3d3432f63424ede
emcaii <emcai@ucsd.edu>
2025-11-15 23:15:25 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZGV2ZWxvcHNnIiwiYSI6ImNsdmxpMnhwaDAwZTkya3FvcjV2NXdyeDcifQ.CZsX2VQBKzIBjvAETaRQuk";
 
